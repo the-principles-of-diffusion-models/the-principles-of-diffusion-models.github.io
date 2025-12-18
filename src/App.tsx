9bdcb134e0dbcc25325a25c4f536675b4fa94869
ChiehHsin-Jesse-Lai <chiehhsinlai@gmail.com>
2025-12-18 14:53:49 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import BlogPost from './pages/BlogPost';
 import ReadOnline from './pages/ReadOnline';
 import Publisher from './pages/Publisher';
 import TeachingGuide from './pages/TeachingGuide';
+import Errata from "./pages/Errata";
 import { trackVisitor } from './lib/visitorTracking';
 
 function App() {
@@ -20,9 +21,11 @@ function App() {
         <Route path="/read-online" element={<ReadOnline />} />
         <Route path="/publisher" element={<Publisher />} />
         <Route path="/teaching" element={<TeachingGuide />} />
+        <Route path="/errata" element={<Errata />} />
       </Routes>
     </HashRouter>
   );
 }
 
 export default App;
+
